0e9d7f4cd42ace5344c2f991daf08cae164c4b6f
sazad44 <530@holbertonschool.com>
2019-02-15 07:23:21 +0000

    Updating console with more methods for more commands

--- console.py
+++ console.py
@@ -73,11 +73,12 @@ class HBNBCommand(cmd.Cmd):
             print(objdict["{}.{}".format(arg_tup[0], arg_tup[1])].__str__())
 
     def help_show(self):
-        """Displays help information for the show command"""
-        print("Displays an object's string representation based on the objects class and id")
+        """Display help information for the show command"""
+        print("Displays an object's string repr based on the object's id")
 
     def do_destroy(self, arg):
         """Deletes instance based on class name and id updating JSON file"""
+        objdict = FileStorage()._FileStorage__objects
         arg_tup = parse(arg)
         classes = ["BaseModel"]
         if len(arg_tup) == 0:
@@ -86,6 +87,56 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
         elif len(arg_tup) == 1:
             print("** instance id missing **")
-        elif 
+        elif "{}.{}".format(arg_tup[0], arg_tup[1]) not in objdict.keys():
+            print("** no instance found**")
+        else:
+            del objdict["{}.{}".format(arg_tup[0], arg_tup[1])]
+            FileStorage().save()
+
+    def help_destroy(self):
+        """Display info about destroy command"""
+        print("Destroys an object based on id and updates the JSON file")
+
+    def do_all(self, arg):
+        """all method displays string representations of all instances"""
+        arg_tup = parse(arg)
+        classes = ["BaseModel"]
+        if len(arg_tup) > 0 and arg_tup[0] not in classes:
+            print("** class doesn't exist **")
+        else:
+            objl = []
+            for obj in FileStorage()._FileStorage__objects.values():
+                objl.append(obj.__str__())
+            print(objl)
+
+    def help_all(self):
+        """Display help information for the all command"""
+        print("Prints a list of string reprs of all objects instantiated")
+
+    def do_update(self, arg):
+        """Updates instance based on id by adding or updating attribute"""
+        arg_tup = parse(arg)
+        classes = ["BaseModel"]
+        objdict = FileStorage()._FileStorage__objects
+        if len(arg_tup) == 0:
+            print("** class name is missing **")
+        elif arg_tup[0] not in classes:
+            print("** class doesn't exist **")
+        elif len(arg_tup) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(arg_tup[0], arg_tup[1]) not in objdict.keys():
+            print("** no instance found **")
+        elif len(arg_tup) == 2:
+            print("** attribute name missing **")
+        elif len(arg_tup) == 3:
+            print("** value missing **")
+        else:
+            obj = objdict["{}.{}".format(arg_tup[0], arg_tup[1])]
+            if arg_tup[2] in obj.__dict__.keys():
+                valtype = type(obj.__dict__[arg_tup[2]])
+                obj.__dict__[arg_tup[2]] = valtype(arg_tup[3])
+            else:
+                obj.__dict__[arg_tup[2]] = arg_tup[3]
+
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
