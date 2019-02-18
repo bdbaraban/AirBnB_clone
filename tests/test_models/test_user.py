7217ecae866ba9a44906e90b39781959fd50569b
bdbaraban <375@holbertonschool.com>
2019-02-18 23:50:30 +0000

    Add unittests for User class attributes

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -32,6 +32,18 @@ class TestUser_instantiation(unittest.TestCase):
     def test_updated_at_is_public_datetime(self):
         self.assertEqual(datetime, type(User().updated_at))
 
+    def test_email_is_public_str(self):
+        self.assertEqual(str, type(User.email))
+
+    def test_password_is_public_str(self):
+        self.assertEqual(str, type(User.password))
+
+    def test_first_name_is_public_str(self):
+        self.assertEqual(str, type(User.first_name))
+
+    def test_last_name_is_public_str(self):
+        self.assertEqual(str, type(User.last_name))
+
     def test_two_users_unique_ids(self):
         us1 = User()
         us2 = User()
