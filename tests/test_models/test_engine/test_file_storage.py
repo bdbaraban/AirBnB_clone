08cdd8a1c3adf28bd6c7ebb9eea2c626342b6cf8
sazad44 <530@holbertonschool.com>
2019-02-20 17:49:05 +0000

    Updating file storage tests to only test if a list has a certain number of elements

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -69,7 +69,8 @@ class TestFileStorage_methods(unittest.TestCase):
     def test_all(self):
         objs = models.storage.all()
         self.assertEqual(type(objs), dict)
-        self.assertEqual(str, type(list(objs.keys())[0]))
+        if len(objs.keys()) > 0:
+            self.assertEqual(str, type(list(objs.keys())[0]))
         self.assertIsInstance(list(objs.values())[0], BaseModel)
 
     def test_all_with_arg(self):
