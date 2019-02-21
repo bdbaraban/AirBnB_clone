9aff2bb10494095d238569537b3252f8e98f1dd3
bdbaraban <375@holbertonschool.com>
2019-02-21 00:33:38 +0000

    Fix console handling of commands involving brackets

--- console.py
+++ console.py
@@ -14,14 +14,20 @@ from models.review import Review
 
 
 def parse(arg):
-    match = re.search(r"\{(.*?)\}", arg)
-    if match is None:
-        lexer = split(arg)
-        return [i.strip(",") for i in lexer]
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
     else:
-        lexer = split(arg[:match.span()[0]])
+        lexer = split(arg[:curly_braces.span()[0]])
         retl = [i.strip(",") for i in lexer]
-        retl.append(match.group())
+        retl.append(curly_braces.group())
         return retl
 
 
