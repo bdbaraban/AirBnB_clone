4c5e0630366ecd6f5917c7ce0c62a9e4750c1980
bdbaraban <375@holbertonschool.com>
2019-02-21 00:42:20 +0000

    Fix update to only typecast strs, ints, and floats

--- console.py
+++ console.py
@@ -197,7 +197,8 @@ class HBNBCommand(cmd.Cmd):
         elif type(eval(argl[2])) == dict:
             obj = objdict["{}.{}".format(argl[0], argl[1])]
             for k, v in eval(argl[2]).items():
-                if k in obj.__class__.__dict__.keys():
+                if (k in obj.__class__.__dict__.keys() and
+                        type(obj.__class__.__dict__[k]) in {str, int, float}):
                     valtype = type(obj.__class__.__dict__[k])
                     obj.__dict__[k] = valtype(v)
                 else:
